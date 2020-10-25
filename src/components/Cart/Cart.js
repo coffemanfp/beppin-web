8a2e3764e690b5f70cb3c6c0273fb9f68f9bff0b
CoffeManFP <coffemanfp@gmail.com>
2020-10-25 12:15:06 -0400

    Details

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,21 +11,26 @@ export default function Cart () {
       name: products[0].name,
       price: products[0].price,
       currency: products[0].currency,
-      image: products[0].images[0].url
+      image: products[0].images[0].url,
+      quantity: 3,
+      quantityAvailable: 12
     },
     {
       id: products[1].id,
       name: products[1].name,
       price: products[1].price,
       currency: products[1].currency,
-      image: products[1].images[0].url
+      image: products[1].images[0].url,
+      quantity: 2,
+      quantityAvailable: 20
     },
     {
       id: products[3].id,
       name: products[3].name,
       price: products[3].price,
       currency: products[3].currency,
-      image: products[3].images[0].url
+      image: products[3].images[0].url,
+      quantityAvailable: 100
     }
   ]
 
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -16,25 +16,33 @@ export default function MainHeader ({ logo, typeAside, triggerAside }) {
     }
   }
 
+  const options = [
+    {
+      isActived: isAsideActived,
+      handleClick: toggleAside,
+      icon: (isAsideActived) ? 'times' : 'bars'
+    },
+    {
+      isActived: typeAside === 'cart',
+      handleClick: () => triggerAside('cart'),
+      icon: 'shopping-cart'
+    }
+  ]
+
   return (
     <header className='main-header'>
       <Logo logo={logo} />
       <Search />
       <div className='main-header__options'>
-        <Button
-          className={`main-header__option ${(isAsideActived) ? 'main-header__option--actived' : ''}`}
-          type='icon'
-          onClick={toggleAside}
-        >
-          <span className={`fal fa-${(isAsideActived) ? 'times' : 'bars'}`} />
-        </Button>
-        <Button
-          className={`main-header__option ${(typeAside === 'cart') ? 'main-header__option--actived' : ''}`}
-          type='icon'
-          onClick={() => triggerAside('cart')}
-        >
-          <span className='fal fa-shopping-cart' />
-        </Button>
+        {options.map((option, i) =>
+          <Button
+            key={i}
+            className={`main-header__option ${(option.isActived) ? 'main-header__option--actived' : ''}`}
+            onClick={option.handleClick}
+          >
+            <span className={`fal fa-${option.icon}`} />
+          </Button>
+        )}
       </div>
     </header>
   )
